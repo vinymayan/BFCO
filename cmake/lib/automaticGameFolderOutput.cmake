8c9a5e5f7e10e999c8fab126927e82bd54b0d3e0
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2024-10-14 17:49:55 +0200

    Update automaticGameFolderOutput.cmake

--- cmake/lib/automaticGameFolderOutput.cmake
+++ cmake/lib/automaticGameFolderOutput.cmake
@@ -1,5 +1,5 @@
 
-function(automaticGameFolderOutput wildlander_output steam_owrt_output steam_mods_output)
+function(automaticGameFolderOutput wildlander_output steam_owrt_output steam_mods_output lorerim_mods_output)
 
     if(DEFINED ENV{WILDLANDER_OWRT_FOLDER} AND IS_DIRECTORY "$ENV{WILDLANDER_OWRT_FOLDER}" AND wildlander_output)
         copyOutputs("$ENV{WILDLANDER_OWRT_FOLDER}/${PRODUCT_NAME}${PROJECT_SUFFIX}")
@@ -12,6 +12,9 @@ function(automaticGameFolderOutput wildlander_output steam_owrt_output steam_mod
     endif()
     if(DEFINED ENV{SKYRIM_MODS_FOLDER2} AND IS_DIRECTORY "$ENV{SKYRIM_MODS_FOLDER2}" AND steam_mods_output)
         copyOutputs("$ENV{SKYRIM_MODS_FOLDER2}/${PRODUCT_NAME}${PROJECT_SUFFIX}")
+    endif()
+    if(DEFINED ENV{LORERIM_MODS_FOLDER} AND IS_DIRECTORY "$ENV{LORERIM_MODS_FOLDER}" AND lorerim_mods_output)
+        copyOutputs("$ENV{LORERIM_MODS_FOLDER}/${PRODUCT_NAME}${PROJECT_SUFFIX}")
     endif()
 
 endfunction()
